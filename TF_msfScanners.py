968346e574e0af127a05a0eb9be72f09dbb22e11
Your Name <you@example.com>
2021-10-28 06:24:00 -0400

    fixed

--- TF_msfScanners.py
+++ TF_msfScanners.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python3
 import sys
 import re 
-import glob, os
+import glob
+import os
 
+def main():
+	proto_list = {
+		"IMAP": [143, 993],
+		"MySql": [3306],
+		"NFS": [2049],
+		"DNS": [53],
+		"SMTP": [25,465,587],
+		"FTP": [21],
+		"WinRM": [5585],
+		"MSSQL": [1433],
+		"Netbios": [137,138, 139],
+		"SMNP": [161],
+		"Telnet": [23],
+		"SMB": [137, 138, 139],
+		"POP3": [110],
+		"SSH": [22],
+		"NTP": [123],
+		"RDP": [3389]
+	}
 
-#these are the protos that tire fire has listed but are not in msfs ["WHOIS",43, ],["LDAP",389, 636, ],["Web",80, 443, ],["Ident",113, ] ,["Kerberos",88, ],["Java-RMI",1098, ],["Portmapper",43, ,["Oracle",1521,],
+	find_proto(proto_list)
 
-PL = [["IMAP",143, 993,], ["MySql",3306,],["NFS",2049, ],["DNS",53],["SMTP",25, 465, 587, ],["FTP", 21, ],["WinRM",5985, ],["MSSQL",1433, ],["Netbios",137, 138, 139],["SNMP",161, ],["Telnet",23, ], ["SMB",137, 138, 139, ],["POP3",110, ],["SSH",22, ],["NTP",123, ],["RDP",3389, ],]
-list_rb = []
+def find_proto(proto_list: dict) -> list:
+	"""these are the protos that tire fire has listed but are not in msfs ["WHOIS",43, ],["LDAP",389, 636, ],["Web",80, 443, ],["Ident",113, ] ,["Kerberos",88, ],["Java-RMI",1098, ],["Portmapper",43, ,["Oracle",1521,]
+	"""
+	for proto_name, proto_ports in proto_list.items():
+		command_list = []
+		proto_dir = "/usr/share/metasproto_listoit-framework/modules/auxiliary/scanner/" + proto_name.lower()
+		rb_files = glob.glob(proto_dir + "*.rb")
 
-#proto = input("Enter search term")
+		for f in rb_files:
+			for port in proto_ports:
+				command = f"msfconsole -q -x 'use {f[:-3]}; set RHOSTS 192.168.152.132; set RPORT {str(port)}; run; exit'"
 
+				command_list.append(command)
 
-def find_proto():
-	for i in range(len(PL)):
-		proto_port= PL[i][1]
-		proto_name=PL[i][0]
-		print(proto_name)
-		print(proto_port)
-		print("/usr/share/metasploit-framework/modules/auxiliary/scanner/"+ str.lower(proto_name))
-		pattern_dir = "/usr/share/metasploit-framework/modules/auxiliary/scanner/"+str.lower(proto_name) #input("Enter pattern dir")
+		with open("/tmp/" + proto_name, "w") as f:
+			for i in command_list:
+				f.write(i + "\n")
 
-
-		if proto_name == proto_name:
-			os.chdir(pattern_dir)
-
-			list_proto = []
-			for file in glob.glob("*.rb"):
-				file = file[:-3]
-				list_proto.append("msfconsole -q -x 'use " + file + "; set RHOSTS 192.168.152.132; set RPORT " + str(proto_port) +"; run; exit' &&")
-				list_rb.append(file)
-				listToStrProto = ' '.join([str(elem) for elem in list_proto]) 
-				outfile = open("/re/Projects/enmu/"+ str.lower(proto_name), "w")
-				outfile.write(listToStrProto)
-				outfile.write("RE")
-				outfile.close()
-			list_proto = []
-
-
-
-		
-
-
-find_proto()
+if __name__ == "__main__":
+	main()
 	
